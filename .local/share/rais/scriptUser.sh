edd1ef76a96ccd23c170ce004f9996b8978f2bf3
Rafael Madriz <rafa@rafaelmadriz.com>
2021-02-24 23:46:46 -0300

    added script to install setup

---
+++ .local/share/rais/scriptUser.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+user_does() {
+    repo="https://github.com/rafamadriz/dotfiles.git"
+    git_dir=".dotfiles"
+    dir_tmp=$(mktemp -d)
+
+    # make directory for fonts
+    mkdir -p "$HOME"/.local/share/fonts
+
+    # clone repository
+    git clone --recurse-submodules --separate-git-dir="$HOME"/$git_dir $repo "$dir_tmp" >/dev/null 2>&1;
+
+    # copy all dotfiles to $HOME (this will overwrite any existing destination file)
+    rsync --recursive --exclude '.git' "$dir_tmp"/ "$HOME"/ >/dev/null 2>&1;
+    rm --force --recursive "$dir_tmp" >/dev/null 2>&1;
+
+    # git set-up for dotfiles
+    function config {
+       /usr/bin/git --git-dir="$HOME"/$git_dir/ --work-tree="$HOME" "$@"
+    }
+    config config status.showUntrackedFiles no
+
+    # junegunn/vim-plug for managin plugins
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' >/dev/null 2>&1
+
+    # install neovim's plugins
+    nvim --headless +PlugInstall +qall >/dev/null 2>&1
+}
+user_does
