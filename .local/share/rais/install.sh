18f797bed4309a38a26be0f65d1f31d2df4229e9
Rafael Madriz <rafa@rafaelmadriz.com>
2021-02-25 14:34:19 -0300

    minor changes

--- .local/share/rais/install.sh
+++ .local/share/rais/install.sh
@@ -10,21 +10,22 @@ echo "== Installing dotfiles... =="
 echo "============================"
 
 pacman -Syy
-pacman -Syu
+pacman_install() {
+   pacman -S --noconfirm --needed "$pkg"
+}
 
-userdo() { sudo --user "$user" }
-
-pacman_install() { pacman -S --noconfirm --needed }
-
-aur_install() { paru -S --aur --needed --noconfirm --removemake }
+aur_install() {
+   paru -S --aur --needed --noconfirm --removemake "$pkg_aur"
+}
 
 install_aurHelper() {
    directory="tmp"
    aur_repo="https://aur.archlinux.org/paru.git"
 
-   pacman -Q git  || pacman_install git
-   userdo git clone "$aur_repo" "$directory"
-   (cd "$directory" && userdo makepkg -sri --noconfirm)
+   pkg="git"
+   pacman -Qn git  || pacman_install $pkg
+   sudo --user "$user" git clone "$aur_repo" "$directory"
+   (cd "$directory" && sudo --user "$user" makepkg -sri --noconfirm)
    rm -rf "$directory"
 }
 
@@ -32,13 +33,13 @@ install_depencies() {
    # Official repositories dependencies
    dependencies=($(sed -n '/name/p' ./packages.txt | awk '{print $2}'))
    for pkg in "${dependencies[@]}"; do
-      pacman -Qn "$pkg" | pacman_install "$pkg"
+      pacman -Qn "$pkg" || pacman_install "$pkg"
    done
 
    # AUR dependencies
    dependencies_aur=($(sed -n '/aur/p' ./packages.txt | awk '{print $2}'))
    for pkg_aur in "${dependencies_aur[@]}"; do
-      pacman -Qm "$pkg_aur" || aur_install "$pkg_aur"
+      pacman -Qm "$pkg_aur" || sudo --user "$user" aur_install "$pkg_aur"
    done
 }
 
@@ -49,16 +50,11 @@ root_does() {
    # Make pacman and paru colorful and adds eye candy on the progress bar.
    grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
    grep -q "^ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
-
-   # trying to improve font rendering
-   ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
-   ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-   ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
 }
 root_does
 
 # run scriptUser
-userdo ./scriptUser.sh
+sudo --user "$user" ./scriptUser.sh
 
 # vimrc config for root user.
 # Useful to have some nice vim defaults when editing files as root
