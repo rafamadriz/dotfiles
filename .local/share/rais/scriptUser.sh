abaa720ed5577c151d297981df72652e6b2660dc
Rafael Madriz <rafa@rafaelmadriz.com>
2021-02-27 00:13:41 -0300

    minor changes

--- .local/share/rais/scriptUser.sh
+++ .local/share/rais/scriptUser.sh
@@ -24,6 +24,8 @@ user_does() {
     # setting up betterlockscreen
     betterlockscreen -u "$HOME"/.local/share/wall/firewatch.jpg
 
+    mkdir -p "$HOME"/.local/share/zsh
+
     # junegunn/vim-plug for managin plugins
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' >/dev/null 2>&1
