4378b80b8bccc7fc3a8fcc751ff9fca11dbb7875
Rafael Madriz <rafa@rafaelmadriz.com>
2021-02-27 16:07:29 -0300

    minor changes

--- .local/share/rais/install.sh
+++ .local/share/rais/install.sh
@@ -2,12 +2,23 @@
 
 [[ $(id -u) -eq 0 ]] || { echo >&2 "Must be root to run script"; exit 1; }
 
-# who's the user running the script ?
-[ "$SUDO_USER" ] && user=$SUDO_USER || user=$(whoami)
-
 echo "============================"
 echo "== Installing dotfiles... =="
 echo "============================"
+
+# who's the user running the script ?
+[ "$SUDO_USER" ] && user=$SUDO_USER || user=$(whoami)
+
+mkdir rais  cd rais || exit
+
+dotscript="https://raw.githubusercontent.com/rafamadriz/dotfiles/main/.local/share/rais/scriptUser.sh"
+pkgs_list="https://raw.githubusercontent.com/rafamadriz/dotfiles/main/.local/share/rais/packages.txt"
+
+for url in $dotscript $pkgs_list; do
+    curl -OL $url
+done
+
+chmod +x "$dotscript"
 
 pacman -Syy
 pacman_install() {
@@ -33,7 +44,7 @@ install_depencies() {
    done
 
    # AUR dependencies
-   dependencies_aur=($(sed -n '/name/p' packages.txt | cut -d' ' -f2))
+   dependencies_aur=($(sed -n '/aur/p' packages.txt | cut -d' ' -f2))
    for pkg_aur in "${dependencies_aur[@]}"; do
       sudo --user "$user" paru -S --aur --needed --noconfirm --removemake "$pkg_aur"
    done
@@ -46,18 +57,18 @@ root_does() {
    # Make pacman and paru colorful and adds eye candy on the progress bar.
    grep -q "^Color" /etc/pacman.conf || sed -i "s/^#Color$/Color/" /etc/pacman.conf
    grep -q "^ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
+
+   # run scriptUser
+   sudo --user "$user" ./dotScript.sh
+
+   # vimrc config for root user.
+   # Useful to have some nice vim defaults when editing files as root
+   ln -s --force /home/"$user"/.local/share/misc0/vimrc /root/.vimrc
+
+   # change shell to zsh
+   [ "$(echo "$SHELL")" != "/usr/bin/zsh" ] && chsh -s /usr/bin/zsh "$user"
 }
 root_does
-
-# run scriptUser
-sudo --user "$user" ./scriptUser.sh
-
-# vimrc config for root user.
-# Useful to have some nice vim defaults when editing files as root
-ln -s --force /home/"$user"/.local/share/misc0/vimrc /root/.vimrc
-
-# change shell to zsh
-[ "$(echo "$SHELL")" != "/usr/bin/zsh" ] && chsh -s /usr/bin/zsh "$user"
 
 echo "=========="
 echo "== Done =="
