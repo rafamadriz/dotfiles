4f619b337d459fa7a7b3a63db7f765e8c47b6371
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-22 22:11:12 -0300

    minor changes

--- .config/nvim-lua/init.lua
+++ .config/nvim-lua/init.lua
@@ -1,11 +1,17 @@
 -- General settings
 require('plugins')
+require('themes')
 require('settings')
 require('mappings')
 
 -- Plugins
 require('plugins/treesitter')
+require('plugins/nvim-tree')
+require('plugins/bufferline')
+require('plugins/statusline')
+require('plugins/startify')
+require('plugins/indent_guides')
 
 -- LSP
-require('lsp/lua')
+require('lsp')
 require('lsp/completion')
