acd267f6b478ee04951fe4e79527bc5b7256cdf6
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-22 22:11:55 -0300

    minor changes

--- .config/nvim-lua/lua/lsp/init.lua
+++ .config/nvim-lua/lua/lsp/init.lua
@@ -1,26 +1,58 @@
-local on_attach = require'completion'.on_attach
+local on_attach = require'utils'.commom_on_attach
+
 -- npm i -g bash-language-server
-require'lspconfig'.bashls.setup{ on_attach=on_attach }
+require'lspconfig'.bashls.setup{ on_attach = on_attach }
 
 -- npm i -g typescript typescript-language-server
 require'lspconfig'.tsserver.setup{ on_attach = on_attach }
 
 -- npm i -g vscode-css-languageserver-bin
-require'lspconfig'.cssls.setup{}
+require'lspconfig'.cssls.setup{ on_attach = on_attach }
 
 -- npm i -g vscode-html-languageserver-bin
-require'lspconfig'.html.setup{}
+require'lspconfig'.html.setup{ on_attach = on_attach }
 
 -- npm i -g pyright
-require'lspconfig'.pyright.setup{}
+require'lspconfig'.pyright.setup{ on_attach = on_attach }
 
 -- npm i -g vscode-json-languageserver
-require'lspconfig'.jsonls.setup {
-    commands = {
-      Format = {
-        function()
-          vim.lsp.buf.range_formatting({},{0,0},{vim.fn.line("$"),0})
-        end
-      }
-    }
+require'lspconfig'.jsonls.setup { on_attach = on_attach }
+
+-- lua  https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
+-- install instructions:
+-- git clone https://github.com/sumneko/lua-language-server $HOME/.cache/nvim/nlua/sumneko_lua
+-- cd ~/.cache/nvim/nlua/sumneko_lua
+-- git submodule update --init --recursive
+-- cd 3rd/luamake
+-- ninja -f ninja/linux.ninja
+-- cd ../..
+-- ./3rd/luamake/luamake rebuild
+local sumneko_root_path = '/home/rafa/.cache/nvim/nlua/sumneko_lua'
+local sumneko_binary = sumneko_root_path..'/bin/Linux/lua-language-server'
+
+require'lspconfig'.sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';'),
+      },
+      completion = { enable = true },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+        },
+      },
+    },
+  },
 }
+require'lspconfig'.sumneko_lua.setup{ on_attach = on_attach }
