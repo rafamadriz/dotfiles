25ca6d0db02a27504939792ccb1273568e52c7b5
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-22 22:12:06 -0300

    minor changes

--- .config/nvim-lua/lua/mappings.lua
+++ .config/nvim-lua/lua/mappings.lua
@@ -5,9 +5,12 @@ vim.g.mapleader = ' '
 
 -- Autocommands
 cmd[[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]]
-cmd[[au BufNewFile,BufRead *.ejs set filetype=html,js]]
+cmd[[au BufNewFile,BufRead *.ejs set filetype=html]]
 cmd[[au FileType markdown let g:indentLine_enabled=0]]
+-- Automatically deletes all trailing whitespace and newlines at end of file on save.
 cmd[[au BufWritePre * %s/\s\+$//e]]
+cmd[[au BufWritePre * %s/\n\+\%$//e]]
+cmd[[au BufWritePre *.[ch] %s/\%$/\r/e]]
 
 -- Basics
 u.map('n', '<leader>w', ':w<CR>')
@@ -33,3 +36,14 @@ u.map('n', 'bp', ':bp<CR>')
 
 -- File manager
 u.map('n', '<leader>e', ':NvimTreeToggle<CR>')
+
+-- LSP
+u.map('n', 'gd', ':lua vim.lsp.buf.definition()<CR>')
+u.map('n', 'gD', ':lua vim.lsp.buf.declaration()<CR>')
+u.map('n', 'gt', ':lua vim.lsp.buf.type_definition()<CR>')
+u.map('n', 'gr', ':lua vim.lsp.buf.references()<CR>')
+u.map('n', 'K', ':lua vim.lsp.buf.hover()<CR>')
+u.map('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>')
+u.map('n', '<space>rn', ':lua vim.lsp.buf.rename()<CR>')
+u.map('n', '<C-p>', ':lua vim.lsp.diagnostic.goto_prev()<CR>')
+u.map('n', '<C-n>', ':lua vim.lsp.diagnostic.goto_next()<CR>')
