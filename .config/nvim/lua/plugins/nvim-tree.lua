6b88a1f4bb442927d21b346edac9115da39cb7b1
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-24 21:24:27 -0300

    plugins

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,40 @@
+vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_disable_netrw = 0
+vim.g.nvim_tree_hijack_netrw = 1
+vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_auto_ignore_ft = "startify"
+vim.g.nvim_tree_side = "right"
+vim.g.nvim_tree_quit_on_open = 0
+vim.g.nvim_tree_disable_netrw = 0
+
+local tree_cb = require "nvim-tree.config".nvim_tree_callback
+vim.g.nvim_tree_bindings = {
+    -- mappings
+    ["<CR>"] = tree_cb("edit"),
+    ["o"] = tree_cb("edit"),
+    ["<2-LeftMouse>"] = tree_cb("edit"),
+    ["<2-RightMouse>"] = tree_cb("cd"),
+    ["<C-]>"] = tree_cb("cd"),
+    ["v"] = tree_cb("vsplit"),
+    ["s"] = tree_cb("split"),
+    ["<C-t>"] = tree_cb("tabnew"),
+    ["h"] = tree_cb("close_node"),
+    ["<BS>"] = tree_cb("close_node"),
+    ["<S-CR>"] = tree_cb("close_node"),
+    ["<Tab>"] = tree_cb("preview"),
+    ["I"] = tree_cb("toggle_ignored"),
+    ["H"] = tree_cb("toggle_dotfiles"),
+    ["R"] = tree_cb("refresh"),
+    ["a"] = tree_cb("create"),
+    ["d"] = tree_cb("remove"),
+    ["r"] = tree_cb("rename"),
+    ["<C-r>"] = tree_cb("full_rename"),
+    ["x"] = tree_cb("cut"),
+    ["c"] = tree_cb("copy"),
+    ["p"] = tree_cb("paste"),
+    ["[c"] = tree_cb("prev_git_item"),
+    ["]c"] = tree_cb("next_git_item"),
+    ["-"] = tree_cb("dir_up"),
+    ["q"] = tree_cb("close")
+}
---
+++ .config/nvim/lua/plugins/startify.lua
@@ -0,0 +1,48 @@
+vim.g.header_ascii = {
+    "     ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+    "     ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+    "     ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+    "     ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+    "     ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+    "     ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ "
+}
+
+--vim.g.startify_bookmarks = {
+--             { 'i': '~/.config/nvim/init.vim' },
+--             { 'p': '~/Documents/Programming/' },
+--             { 'z': '~/.config/zsh/.zshrc' },
+--             { 't': '~/.config/kitty/kitty.conf' },
+--             }
+--
+--vim.g.startify_lists = {
+--           { 'type': 'files',     'header': ['   Files']                        },
+--           { 'type': 'dir',       'header': ['   Current Directory '. getcwd()] },
+--           { 'type': 'sessions',  'header': ['   Sessions']                     },
+--           { 'type': 'bookmarks', 'header': ['   Bookmarks']                    },
+--           }
+
+vim.api.nvim_exec(
+    "let startify_lists = [ { 'type': 'files',     'header': ['   Files'] }, { 'type': 'dir',     'header': ['   Current Directory '.getcwd()] }, { 'type': 'sessions',  'header': ['   Sessions'] },    { 'type': 'bookmarks', 'header': ['   Bookmarks'] },                                                                   ]",
+    true
+)
+
+vim.api.nvim_exec(
+    "let startify_bookmarks = [ { 'i': '~/.config/nvim/init.lua' }, { 'z': '~/.config/zsh/.zshrc' } ]",
+    true
+)
+
+vim.g.startify_session_dir = "~/.config/nvim/session"
+
+-- start Vim in a directory that contains a Session.vim, that session will be loaded automatically
+vim.g.startify_session_autoload = 1
+-- Let Startify take care of buffers
+vim.g.startify_session_delete_buffers = 1
+-- Similar to Vim-rooter
+vim.g.startify_change_to_vcs_root = 1
+-- Unicode
+vim.g.startify_fortune_use_unicode = 1
+-- Automatically update sessions
+vim.g.startify_session_persistence = 1
+-- Get rid of empy buffer on quit
+vim.g.startify_enable_special = 0
+vim.g.startify_custom_header = "startify#center(g:header_ascii)"
