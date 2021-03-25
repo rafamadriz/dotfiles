23c1833527e10def02a788b83074440d127374d9
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-25 11:20:41 -0300

    minor changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,18 +4,17 @@ require("mappings")
 require("plugins")
 require("themes")
 
+-- LSP
+require("lsp")
+require("lsp/completion")
+
 -- Plugins
 require("plugins/general")
-require("plugins/treesitter")
 require("plugins/nvim-tree")
-require("plugins/bufferline")
 require("plugins/statusline")
 require("plugins/startify")
 require("plugins/indent-guides")
 require("plugins/toggleterm")
 require("plugins/telescope")
 require("plugins/emmet")
-
--- LSP
-require("lsp")
-require("lsp/completion")
+require("plugins/treesitter")
