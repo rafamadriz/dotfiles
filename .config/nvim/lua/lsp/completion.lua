3044c6f4b9da4a695921753d8bf5166b456a7640
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-24 21:27:40 -0300

    minor changes

--- .config/nvim/lua/lsp/completion.lua
+++ .config/nvim/lua/lsp/completion.lua
@@ -1,21 +1,45 @@
-local u = require('utils')
+local u = require("utils")
 
 -- TODO figure out why this don't work
-vim.fn.sign_define("LspDiagnosticsSignError",
-                   {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"})
-vim.fn.sign_define("LspDiagnosticsSignWarning",
-                   {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"})
-vim.fn.sign_define("LspDiagnosticsSignInformation",
-                   {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"})
-vim.fn.sign_define("LspDiagnosticsSignHint",
-                   {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"})
+vim.fn.sign_define(
+    "LspDiagnosticsSignError",
+    {
+        texthl = "LspDiagnosticsSignError",
+        text = "",
+        numhl = "LspDiagnosticsSignError"
+    }
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignWarning",
+    {
+        texthl = "LspDiagnosticsSignWarning",
+        text = "",
+        numhl = "LspDiagnosticsSignWarning"
+    }
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignInformation",
+    {
+        texthl = "LspDiagnosticsSignInformation",
+        text = "",
+        numhl = "LspDiagnosticsSignInformation"
+    }
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignHint",
+    {
+        texthl = "LspDiagnosticsSignHint",
+        text = "",
+        numhl = "LspDiagnosticsSignHint"
+    }
+)
 
-require'compe'.setup {
+require "compe".setup {
     enabled = true,
     autocomplete = true,
     debug = false,
     min_length = 1,
-    preselect = 'enable',
+    preselect = "enable",
     throttle_time = 80,
     source_timeout = 200,
     incomplete_delay = 400,
@@ -23,7 +47,6 @@ require'compe'.setup {
     max_kind_width = 100,
     max_menu_width = 100,
     documentation = true,
-
     source = {
         path = {kind = "  "},
         buffer = {kind = "  "},
@@ -41,70 +64,33 @@ require'compe'.setup {
 }
 
 -- symbols for autocomplete
-require('lspkind').init({
-    with_text = false,
-    symbol_map = {
-      Text = '  ',
-      Method = '  ',
-      Function = ' ƒ ',
-      Constructor = '  ',
-      Variable = '[]',
-      Class = '  ',
-      Interface = ' 蘒',
-      Module = '  ',
-      Property = '  ',
-      Unit = ' 塞 ',
-      Value = '  ',
-      Enum = ' 練',
-      Keyword = '  ',
-      Snippet = '  ',
-      Color = '',
-      File = '',
-      Folder = ' ﱮ ',
-      EnumMember = '  ',
-      Constant = '  ',
-      Struct = '  '
-    },
-})
+require("lspkind").init(
+    {
+        with_text = false,
+        symbol_map = {
+            Text = "  ",
+            Method = "  ",
+            Function = " ƒ ",
+            Constructor = "  ",
+            Variable = "[]",
+            Class = "  ",
+            Interface = " 蘒",
+            Module = "  ",
+            Property = "  ",
+            Unit = " 塞 ",
+            Value = "  ",
+            Enum = " 練",
+            Keyword = "  ",
+            Snippet = "  ",
+            Color = "",
+            File = "",
+            Folder = " ﱮ ",
+            EnumMember = "  ",
+            Constant = "  ",
+            Struct = "  "
+        }
+    }
+)
 
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-    local col = vim.fn.col('.') - 1
-    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-        return true
-    else
-        return false
-    end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-n>"
-    elseif vim.fn.call("vsnip#available", {1}) == 1 then
-        return t "<Plug>(vsnip-expand-or-jump)"
-    elseif check_back_space() then
-        return t "<Tab>"
-    else
-        return vim.fn['compe#complete']()
-    end
-end
-_G.s_tab_complete = function()
-    if vim.fn.pumvisible() == 1 then
-        return t "<C-p>"
-    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-        return t "<Plug>(vsnip-jump-prev)"
-    else
-        return t "<S-Tab>"
-    end
-end
-
-u.map("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-u.map("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-u.map("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-u.map("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+u.map("i", "<Tab>", [[ pumvisible() ? "\<C-n>" : "\<Tab>" ]], {expr = true})
+u.map("i", "<S-Tab>", [[ pumvisible() ? "\<C-p>" : "\<S-Tab>" ]], {expr = true})
