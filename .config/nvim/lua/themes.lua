feeef96484634eb8d3cf861d63208c495ef31841
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-24 21:30:17 -0300

    minor changes

---
+++ .config/nvim/lua/themes.lua
@@ -0,0 +1,21 @@
+local cmd = vim.cmd
+
+-- gruvbox settings
+vim.g.gruvbox_contrast_dark = "medium"
+
+-- sonokai settings
+vim.g.sonokai_style = "andromeda"
+vim.g.sonokai_enable_italic = 1
+vim.g.sonokai_transparent_background = 0
+vim.g.sonokai_current_word = "bold"
+vim.g.sonokai_better_performance = 1
+vim.g.sonokai_diagnostic_text_highlight = 1
+
+-- spacevim settings
+vim.g.space_vim_italic = 1
+
+-- onedark
+vim.g.onedark_termcolors = 16
+
+-- Colorscheme
+cmd("colorscheme space_vim_theme")
