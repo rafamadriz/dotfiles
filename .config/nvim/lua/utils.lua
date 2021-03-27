6a5a635bf5b7e738017ea872a7225b19ddd73cd5
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-27 00:38:25 -0300

    clean up

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -32,30 +32,6 @@ function utils.search_dotfiles()
     )
 end
 
--- completion
-local function documentHighlight(client, bufnr)
-    -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec(
-            [[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-            false
-        )
-    end
-end
-
-function utils.common_on_attach(client, bufnr)
-    documentHighlight(client, bufnr)
-end
-
 -- Local
 --- Check if a file or directory exists in this path
 function G.exists(file)
