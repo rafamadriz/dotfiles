b91a7160be726434dc1f77b409fd197562982aa8
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-27 00:36:20 -0300

    minor changes

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 vim.g.nvim_tree_indent_markers = 1
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_disable_netrw = 0
-vim.g.nvim_tree_hijack_netrw = 1
+vim.g.nvim_tree_hijack_netrw = 0
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_auto_ignore_ft = "startify"
 vim.g.nvim_tree_side = "right"
