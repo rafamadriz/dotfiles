999adfb8f7e102cca958011c92c9c7e861226e42
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-27 00:37:40 -0300

    minor changes

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,6 @@
 require "toggleterm".setup {
     size = 20,
-    open_mapping = [[<c-\>]],
+    open_mapping = [[<c-t>]],
     shade_filetypes = {},
     shade_terminals = true,
     shading_factor = "1", -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
