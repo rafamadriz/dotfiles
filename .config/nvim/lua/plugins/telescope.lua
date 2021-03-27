6828fc03266a13487812bc6aa7455dad82becb66
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-27 00:37:00 -0300

    added images preview

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -61,7 +61,14 @@ require("telescope").setup {
         fzy_native = {
             override_generic_sorter = false,
             override_file_sorter = true
+        },
+        media_files = {
+            -- filetypes whitelist
+            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+            filetypes = {"png", "webp", "jpg", "jpeg"},
+            find_cmd = "rg" -- find command (defaults to `fd`)
         }
     }
 }
 require("telescope").load_extension("fzy_native")
+require("telescope").load_extension("media_files")
