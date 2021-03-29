db8c6282397d2924b9a4ee1fd4efe49cf6f6fca6
Rafael Madriz <rafa@rafaelmadriz.com>
2021-03-29 20:42:27 -0300

    new user config

---
+++ .config/nvim/lua/config.lua
@@ -0,0 +1,10 @@
+Theming = {
+    -- Press <space>fc to see all available themes
+    colorscheme = "edge",
+    --[[ Some colorscheme have multiple styles to choose from.
+      here are the available options:
+      For @gruvbox = medium, soft, hard
+      For @edge = default, aura, neon
+      For @sonokai = default, atlantis, andromeda, shusia, maia ]]
+    colorscheme_style = ""
+}
