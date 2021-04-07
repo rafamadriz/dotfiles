d7bafab6af8a6b07a2d0fa3070948e90a94b72e7
Rafael Madriz <rafa@rafaelmadriz.com>
2021-04-07 13:27:51 -0300

    minor changes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,4 +7,5 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar nordblocks 2>&1 | tee -a /tmp/polybar2.log & disown
+polybar nordblocks 2>&1 | tee -a /tmp/polybar2.log &
+disown
