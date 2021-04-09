e246fcbd2ec33966d017ccc4173a369f72bb60ec
Rafael Madriz <rafa@rafaelmadriz.com>
2021-04-09 11:51:59 -0300

    more options for config.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,12 @@
 if Treesitter.enabled == nil or Treesitter.enabled == true then
     require "nvim-treesitter.configs".setup {
-        ensure_installed = {
-            "javascript",
-            "typescript",
-            "tsx",
-            "html",
-            "css",
-            "c",
-            "lua",
-            "bash",
-            "python",
-            "json",
-            "yaml"
-        }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+        ensure_installed = Treesitter.parsers,
         highlight = {
             enable = true, -- false will disable the whole extension
             use_languagetree = true
         },
         indent = {enable = true},
         rainbow = {enable = Treesitter.rainbow},
-        autotag = {enable = Treesitter.autotag}
+        autotag = {enable = Completion.autotag}
     }
 end
