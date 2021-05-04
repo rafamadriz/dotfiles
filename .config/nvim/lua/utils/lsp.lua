0040f7ca41cad636f9f80b0ef75f3def800c3769
Rafael Madriz <rafa@rafaelmadriz.com>
2021-05-04 15:31:19 -0300

    add lsp colors to colorschemes that don't support it

--- .config/nvim/lua/utils/lsp.lua
+++ .config/nvim/lua/utils/lsp.lua
@@ -50,4 +50,77 @@ lsp_config.cmds = {
     tsserver = {DATA_PATH .. "/lspinstall/typescript/node_modules/.bin/typescript-language-server", "--stdio"}
 }
 
+-- Add LSP colors to colorschemes that don't support it yet
+local defaults = {
+    Error = "#db4b4b",
+    Warning = "#e0af68",
+    Information = "#0db9d7",
+    Hint = "#10B981"
+}
+
+local config = {}
+
+function lsp_config.hl(name)
+    return vim.api.nvim_get_hl_by_name(name, true)
+end
+
+function lsp_config.exists(name)
+    if vim.fn.hlexists(name) == 1 then
+        local hl = lsp_config.hl(name)
+        local count = 0
+        for key, value in pairs(hl) do
+            -- this is needed for groups that only have "cleared"
+            if not (key == true and value == 6) then
+                count = count + 1
+            end
+        end
+        return count > 0
+    end
+    return false
+end
+
+function lsp_config.link(group, fallbacks, default)
+    if not lsp_config.exists(group) then
+        for _, fallback in pairs(fallbacks) do
+            if lsp_config.exists(fallback) then
+                vim.cmd("hi link " .. group .. " " .. fallback)
+                return
+            end
+        end
+        if default then
+            vim.cmd("hi " .. group .. " " .. default)
+        end
+    end
+end
+
+function lsp_config.fix()
+    -- dump({ fix = event })
+    -- Default Groups
+    for _, lsp in pairs({"Error", "Warning", "Information", "Hint"}) do
+        local coc = lsp
+        if lsp == "Information" then
+            coc = "Info"
+        end
+        lsp_config.link("LspDiagnosticsDefault" .. lsp, {"Coc" .. coc .. "Sign"}, "guifg=" .. config[lsp])
+        lsp_config.link("LspDiagnosticsVirtualText" .. lsp, {"LspDiagnosticsDefault" .. lsp})
+
+        local color = defaults[lsp]
+        local hl = lsp_config.hl("LspDiagnosticsDefault" .. lsp)
+        if hl and hl.foreground then
+            color = string.format("#%06x", hl.foreground)
+        end
+        lsp_config.link("LspDiagnosticsUnderline" .. lsp, {}, "gui=undercurl guisp=" .. color)
+    end
+
+    lsp_config.link("LspReferenceText", {"CocHighlightText", "CursorLine"})
+    lsp_config.link("LspReferenceRead", {"CocHighlightRead", "LspReferenceText"})
+    lsp_config.link("LspReferenceWrite", {"CocHighlightWrite", "LspReferenceText"})
+end
+
+function lsp_config.setup(options)
+    config = vim.tbl_deep_extend("force", {}, defaults, options or {})
+end
+
+lsp_config.setup()
+
 return lsp_config
