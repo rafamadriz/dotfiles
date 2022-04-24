055e7b6e48216ff8646037c0502cd660177c1a69
Rafael Madriz <rafa@rafaelmadriz.com>
2022-04-24 13:39:33 -0300

    ref(nvim): treesitter `maintained` no longer supported

--- private_dot_config/nvim/lua/modules/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/modules/plugins/treesitter.lua
@@ -3,7 +3,7 @@ local M = {}
 M.config = function()
     if as._default(vim.g.code_treesitter_enabled) then
         require("nvim-treesitter.configs").setup {
-            ensure_installed = vim.g.code_treesitter_parsers_install or "maintained",
+            ensure_installed = vim.g.code_treesitter_parsers_install or "all",
             ignore_install = vim.g.code_treesitter_parsers_ignore or {},
             highlight = {
                 enable = as._default(vim.g.code_treesitter_highlight), -- false will disable the whole extension
