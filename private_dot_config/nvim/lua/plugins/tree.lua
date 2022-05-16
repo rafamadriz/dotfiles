e48923e5b6e47aacf2a6a3a2dacf6b22ccae867e
Rafael Madriz <rafa@rafaelmadriz.com>
2022-05-15 23:13:12 -0300

    nvim: Add options to nvim-tree
    These options work as an integration for project.nvim
    
    Ref: <https://github.com/ahmedkhalf/project.nvim#-features=>

--- private_dot_config/nvim/lua/plugins/tree.lua
+++ private_dot_config/nvim/lua/plugins/tree.lua
@@ -1,6 +1,10 @@
 require 'nvim-tree'.setup {
     disable_netrw = true, -- disables netrw completely
     update_cwd = true, -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+    update_focused_file = {
+        enable = true,
+        update_cwd = true
+    },
     view = {
         width = 40,
         side = "right",
@@ -23,5 +27,6 @@ require 'nvim-tree'.setup {
 vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_group_empty = 1
+vim.g.nvim_tree_respect_buf_cwd = 1
 
 as.nnoremap('<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Open file tree' })
