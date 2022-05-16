dc42cba781e91121d13883012f18dac62b9ae1be
Rafael Madriz <rafa@rafaelmadriz.com>
2022-05-15 22:25:12 -0300

    nvim: Add `zk` config

---
+++ private_dot_config/nvim/lua/plugins/zk.lua
@@ -0,0 +1,24 @@
+require "zk".setup()
+local mappings = {
+    ["<leader>"] = {
+        ["n"] = {
+            name = "Notes",
+            l = { "<cmd>ZkLinks<CR>", "Zk links" },
+            b = { "<cmd>ZkBackLinks<CR>", "Zk backlinks" },
+            f = { "<cmd>ZkNotes { sort = { 'modified' } }<CR>", "Zk find notes" },
+            c = { "<cmd>ZkCd<CR>", "Zk change directory" },
+            t = { "<cmd>ZkTags<CR>", "Zk tags" },
+            n = { function()
+                vim.ui.input(
+                    { prompt = "Title: " },
+                    function(title)
+                        if title ~= nil then
+                            require "zk".new({ title = title })
+                        end
+                    end
+                )
+            end, "Zk new" },
+        }
+    }
+}
+require("which-key").register(mappings, { mode = "n" })
