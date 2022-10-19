78abe037b7e6e538c67484ff36a8cbdc873038cd
Rafael Madriz <rafa@rafaelmadriz.com>
2022-10-18 22:18:43 -0300

    nvim(formatter): Add `zig` formatter

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -88,7 +88,7 @@ capabilities.textDocument.completion.completionItem.resolveSupport = {
         "additionalTextEdits",
     },
 }
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 local lspconfig = require "lspconfig"
 
