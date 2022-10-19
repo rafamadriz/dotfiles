6bcd92f151bb58e22a8f22a060cb9cab52031318
Rafael Madriz <rafa@rafaelmadriz.com>
2022-10-18 22:19:17 -0300

    nvim(lsp): Fix `update_capabilities` is deprecated
    Use `default_capabilities` instead.
    
    * Remove setting up server capabilities (e.g `snippetSupport`),
    `cmp_nvim_lsp` already does that for you.

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -73,30 +73,13 @@ local on_attach = function(client, bufnr)
     end
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities.textDocument.completion.completionItem.preselectSupport = true
-capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-capabilities.textDocument.completion.completionItem.resolveSupport = {
-    properties = {
-        "documentation",
-        "detail",
-        "additionalTextEdits",
-    },
-}
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+require("lspconfig.ui.windows").default_options.border = as.lsp.borders
 
 local lspconfig = require "lspconfig"
 
-require("lspconfig.ui.windows").default_options.border = as.lsp.borders
-
 lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
     on_attach = on_attach,
-    capabilities = capabilities,
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
 -- Setup ccls with lspconfig since it's not going to be supported by mason.nvim
