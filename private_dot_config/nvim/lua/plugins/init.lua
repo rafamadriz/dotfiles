148a8157f6f5da3df90e657fe2e433e0128df036
Rafael Madriz <rafa@rafaelmadriz.com>
2023-01-20 12:50:10 -0300

    nvim: Add `git` source to `cmp`

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -39,6 +39,7 @@ require("lazy").setup({
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
         dependencies = {
+            { "petertriho/cmp-git", ft = { "NeogitCommitMessage", "gitcommit" } },
             { "hrsh7th/cmp-nvim-lsp" },
             { "hrsh7th/cmp-path" },
             { "hrsh7th/cmp-buffer" },
