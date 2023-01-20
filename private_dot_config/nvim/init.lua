abed1b85e59f459a015a59790febd77e523bf8fd
Rafael Madriz <rafa@rafaelmadriz.com>
2023-01-20 10:58:33 -0300

    nvim: Remove impatien.nvim since it's not needed with lazy.nvim
    Reference: <https://github.com/folke/lazy.nvim/issues/148>

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,18 +1,5 @@
 local fn = vim.fn
-local install_path = fn.stdpath "data" .. "/site/pack/impatient/start/impatient.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-    -- NOTE: this is plugin is unnecessary once https://github.com/neovim/neovim/pull/15436 is merged
-    fn.system {
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "https://github.com/lewis6991/impatient.nvim",
-        install_path,
-    }
-end
 
-require "impatient"
 require "globals"
 require "settings"
 require "plugins"
