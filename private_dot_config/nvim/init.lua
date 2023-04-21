9a4ba793027a27af7bcbb314a6401ee303ed2306
Rafael Madriz <rafa@rafaelmadriz.com>
2023-04-20 21:12:04 -0300

    nvim.init: Disable unused builtin plugins
    Never use those builtin plugins so disable them all

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -4,6 +4,28 @@ vim.keymap.set("v", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- Disable some builtin plugins
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_man = 1
+vim.g.loaded_remote_plugins = 1
+-- vim.g.loaded_matchit = 1
+-- vim.g.loaded_matchparen = 1
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
