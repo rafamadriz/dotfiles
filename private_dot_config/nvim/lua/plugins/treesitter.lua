7af0347ff8161294542290fbdcdc3ee697d9832b
Rafael Madriz <rafa@rafaelmadriz.com>
2023-04-23 20:45:24 -0300

    nvim.treesitter: Move into its own file and and ts related plugins

---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,41 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = ":TSUpdate",
+        dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+        config = function()
+            require("nvim-treesitter.configs").setup {
+                ensure_installed = "all",
+                highlight = {
+                    enable = true,
+                    disable = { "html" },
+                },
+                indent = { enable = false },
+                autotag = { enable = true },
+            }
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        ft = {
+            "html",
+            "javascript",
+            "typescript",
+            "javascriptreact",
+            "typescriptreact",
+            "svelte",
+            "vue",
+            "tsx",
+            "jsx",
+            "rescript",
+            "xml",
+            "php",
+            "markdown",
+            "astro",
+            "glimmer",
+            "handlebars",
+            "hbs",
+        },
+    },
+}
