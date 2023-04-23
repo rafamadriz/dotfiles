c6c46f821c69031499debb76aec39d4f2f0f9796
Rafael Madriz <rafa@rafaelmadriz.com>
2023-04-22 22:38:43 -0300

    nvim.plugins: Add `cmp` for completion

---
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,100 @@
+local lsp_symbols = {
+    Class = "   Class",
+    Color = "   Color",
+    Constant = "   Constant",
+    Constructor = "   Constructor",
+    Enum = " ❐  Enum",
+    EnumMember = "   EnumMember",
+    Event = "   Event",
+    Field = " ﴲ  Field",
+    File = "   File",
+    Folder = "   Folder",
+    Function = "   Function",
+    Interface = " ﰮ  Interface",
+    Keyword = "   Keyword",
+    Method = "   Method",
+    Module = "   Module",
+    Operator = "   Operator",
+    Property = "   Property",
+    Reference = "   Reference",
+    Snippet = " ﬌  Snippet",
+    Struct = " ﳤ  Struct",
+    Text = "   Text",
+    TypeParameter = "   TypeParameter",
+    Unit = "   Unit",
+    Value = "   Value",
+    Variable = "[] Variable",
+}
+
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            { "petertriho/cmp-git", config = true },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-nvim-lua" },
+            -- { "saadparwaiz1/cmp_luasnip" },
+            -- { "hrsh7th/cmp-cmdline" },
+        },
+        config = function()
+            local cmp = require"cmp"
+            cmp.setup {
+                snippet = {
+                    expand = function(args) require("luasnip").lsp_expand(args.body) end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = {
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-c>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Insert,
+                        select = true,
+                    },
+                },
+                formatting = {
+                    format = function(entry, item)
+                        item.kind = lsp_symbols[item.kind]
+                        item.menu = ({
+                            nvim_lsp = "[LSP]",
+                            path = "[F]",
+                            luasnip = "[S]",
+                            buffer = "[B]",
+                            nvim_lua = "[Lua]",
+                            git = "[Git]"
+                        })[entry.source.name]
+                        return item
+                    end,
+                },
+                sources = cmp.config.sources {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    {
+                        name = "buffer",
+                        option = {
+                            get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+                        },
+                    },
+                    { name = "path" },
+                    { name = "nvim_lua" },
+                },
+            }
+          cmp.setup.filetype('gitcommit', {
+            sources = cmp.config.sources({
+              { name = 'git' }, -- You can specify the `cmp_git` source if you were installed it.
+            }, {
+              { name = 'buffer' },
+            })
+          })
+        end
+    }
+}
