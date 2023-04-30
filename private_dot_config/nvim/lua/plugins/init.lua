f0a3128733802df24aaacd894db19429eeffe838
Rafael Madriz <rafa@rafaelmadriz.com>
2023-04-30 11:11:35 -0300

    nvim.plugins: Add undotree

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -66,4 +66,14 @@ return {
         event = { "BufReadPre", "BufNewFile" },
         config = true,
     },
+    {
+        "mbbill/undotree",
+        cmd = "UndotreeToggle",
+        keys = { { "<leader>u", "<cmd>UndotreeToggle<CR>", desc = "Undotree" } },
+        config = function()
+            vim.g.undotree_WindowLayout = 2
+            vim.g.undotree_SplitWidth = 45
+            vim.g.undotree_SetFocusWhenToggle = 1
+        end,
+    },
 }
