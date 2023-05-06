d77cb44899d45c984ab94398313567eae43c7682
Rafael Madriz <rafa@rafaelmadriz.com>
2023-05-06 12:34:54 -0300

    nvim.ai: Add `treesitter` text objects

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -2,8 +2,23 @@ return {
     {
         "echasnovski/mini.ai",
         event = "VeryLazy",
+        dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
         config = function()
-            require("mini.ai").setup { mappings = { around_last = "", inside_last = "" } }
+            local ai = require "mini.ai"
+            require("mini.ai").setup {
+                mappings = { around_last = "", inside_last = "" },
+                custom_textobjects = {
+                    i = ai.gen_spec.treesitter(
+                        { a = "@conditional.outer", i = "@conditional.inner" },
+                        {}
+                    ),
+                    f = ai.gen_spec.treesitter(
+                        { a = "@function.outer", i = "@function.inner" },
+                        {}
+                    ),
+                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+                },
+            }
         end,
     },
     {
