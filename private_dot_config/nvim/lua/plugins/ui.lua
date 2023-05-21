108b01c76e63caa4a77a35cd517cb2a99400da62
Rafael Madriz <rafa@rafaelmadriz.com>
2023-05-21 13:17:11 -0300

    nvim.ui: Remove `dashboard-nvim`

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -1,57 +1,5 @@
 return {
     {
-        "glepnir/dashboard-nvim",
-        event = { "VimEnter" },
-        config = function()
-            require("dashboard").setup {
-                theme = "hyper",
-                shortcut_type = "number",
-                config = {
-                    header = {
-                        [[      ::::    ::: :::::::::: ::::::::  :::     ::: :::::::::::   :::   :::]],
-                        [[     :+:+:   :+: :+:       :+:    :+: :+:     :+:     :+:      :+:+: :+:+:]],
-                        [[    :+:+:+  +:+ +:+       +:+    +:+ +:+     +:+     +:+     +:+ +:+:+ +:+]],
-                        [[   +#+ +:+ +#+ +#++:++#  +#+    +:+ +#+     +:+     +#+     +#+  +:+  +#+ ]],
-                        [[  +#+  +#+#+# +#+       +#+    +#+  +#+   +#+      +#+     +#+       +#+  ]],
-                        [[ #+#   #+#+# #+#       #+#    #+#   #+#+#+#       #+#     #+#       #+#   ]],
-                        [[###    #### ########## ########      ###     ########### ###       ###    ]],
-                        [[]],
-                        [[]],
-                        [[]],
-                    },
-                    shortcut = {
-                        { desc = "Lazy", action = "Lazy", key = "l", group = "Constant" },
-                        { desc = "Mason", action = "Mason", key = "m", group = "Constant" },
-                        {
-                            desc = "Find files",
-                            action = "Telescope find_files",
-                            key = "f",
-                            group = "Constant",
-                        },
-                        {
-                            desc = "Grep string",
-                            action = "Telescope live_grep_args",
-                            key = "g",
-                            group = "Constant",
-                        },
-                        {
-                            desc = "Dotfiles",
-                            action = "Telescope find_files cwd=~/.local/share/chezmoi/",
-                            key = "d",
-                            group = "Constant",
-                        },
-                        {
-                            desc = "Quit",
-                            action = "quitall",
-                            key = "q",
-                            group = "Constant",
-                        },
-                    },
-                },
-            }
-        end,
-    },
-    {
         "nvim-lualine/lualine.nvim",
         event = "VeryLazy",
         config = function()
