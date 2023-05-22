34a5d22ff3f6ce54b45ba277e0ce019726e2a688
Rafael Madriz <rafa@rafaelmadriz.com>
2023-05-21 23:20:04 -0300

    nvim.plugins: Add `lsp_lines.nvim`

--- private_dot_config/nvim/lua/plugins/lspconfig/init.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig/init.lua
@@ -44,4 +44,12 @@ return {
         cmd = { "Glance" },
         config = true,
     },
+    {
+        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+        event = "LspAttach",
+        config = function()
+            require("lsp_lines").setup()
+            vim.diagnostic.config { virtual_lines = { only_current_line = true } }
+        end,
+    },
 }
