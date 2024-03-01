2d087026bd0b92a6c7caac98aeeea628441f2f81
Rafael Madriz <rafa@rafaelmadriz.com>
2024-02-29 21:24:30 -0300

    nvim: colorschem changes

--- private_dot_config/nvim/lua/plugins/colorschemes.lua
+++ private_dot_config/nvim/lua/plugins/colorschemes.lua
@@ -18,10 +18,11 @@ return {
     },
     {
         "sainnhe/gruvbox-material",
+        lazy = false,
         config = function()
             vim.g.gruvbox_material_dim_inactive_windows = 1
             vim.g.gruvbox_material_better_performance = 1
-            vim.g.gruvbox_material_foreground = "material" -- options: material, mix, original
+            vim.g.gruvbox_material_foreground = "original" -- options: material, mix, original
             vim.g.gruvbox_material_background = "medium" -- options: hard, medium, soft
             vim.cmd.colorscheme "gruvbox-material"
         end,
@@ -90,7 +91,6 @@ return {
     },
     {
         "rose-pine/neovim",
-        lazy = false,
         name = "rose-pine",
         config = function()
             require("rose-pine").setup {
