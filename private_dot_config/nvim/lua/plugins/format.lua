abe67469226f100277ec51a7730b31919ac79079
Rafael Madriz <rafa@rafaelmadriz.com>
2024-03-01 08:38:41 -0300

    nvim: move from formatter to conform

--- private_dot_config/nvim/lua/plugins/format.lua
+++ private_dot_config/nvim/lua/plugins/format.lua
@@ -1,24 +1,45 @@
 return {
-    "mhartington/formatter.nvim",
-    cmd = { "Format", "FormatWrite" },
+    "stevearc/conform.nvim",
+    lazy = false,
+    init = function()
+        vim.api.nvim_create_user_command("Format", function(args)
+            local range = nil
+            if args.count ~= -1 then
+                local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                range = {
+                    start = { args.line1, 0 },
+                    ["end"] = { args.line2, end_line:len() },
+                }
+            end
+            require("conform").format { async = true, lsp_fallback = true, range = range }
+        end, { range = true })
+    end,
     config = function()
-        require("formatter").setup {
-            filetype = {
-                c = { require("formatter.filetypes.c").clangformat },
-                css = { require("formatter.filetypes.css").prettier },
-                go = { require("formatter.filetypes.go").goimports },
-                html = { require("formatter.filetypes.html").prettier },
-                javascript = { require("formatter.filetypes.javascript").prettier },
-                javascriptreact = { require("formatter.filetypes.javascriptreact").prettier },
-                json = { require("formatter.filetypes.json").prettier },
-                lua = { require("formatter.filetypes.lua").stylua },
-                markdown = { require("formatter.filetypes.markdown").prettier },
-                rust = { require("formatter.filetypes.rust").rustfmt },
-                sh = { require("formatter.filetypes.sh").shfmt },
-                toml = { require("formatter.filetypes.toml").taplo },
-                typescript = { require("formatter.filetypes.typescript").prettier },
-                typescriptreact = { require("formatter.filetypes.typescriptreact").prettier },
-                zig = { require("formatter.filetypes.zig").zigfmt },
+        require("conform").setup {
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_fallback = true,
+            },
+            formatters_by_ft = {
+                -- Conform will run multiple formatters sequentially
+                -- python = { "isort", "black" },
+                c = { "clangformat" },
+                css = { "prettier" },
+                go = { "goimports" },
+                html = { "prettier" },
+                -- Use a sub-list to run only the first available formatter
+                javascript = { { "prettierd", "prettier" } },
+                javascriptreact = { "prettier" },
+                json = { "prettier" },
+                lua = { "stylua" },
+                markdown = { "prettier" },
+                rust = { "rustfmt" },
+                sh = { "shfmt" },
+                toml = { "taplo" },
+                typescript = { "prettier" },
+                typescriptreact = { "prettier" },
+                zig = { "zigfmt" },
             },
         }
     end,
