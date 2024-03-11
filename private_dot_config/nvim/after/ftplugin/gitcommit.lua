fc1b5a7e978a7335e3b907c20fc01f370f9341c9
Rafael Madriz <rafa@rafaelmadriz.com>
2024-03-10 22:09:50 -0300

    nvim: add config for gitcommit

---
+++ private_dot_config/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,6 @@
+vim.opt_local.list = false
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_gb"
+vim.opt_local.textwidth = 72
