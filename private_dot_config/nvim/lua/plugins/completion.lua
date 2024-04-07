e0df106b6326e7081c2e48cbe9e4a8df94880cc9
Rafael Madriz <rafa@rafaelmadriz.com>
2024-04-06 21:59:55 -0300

    nvim: cmp option `follow_cursor`
    * move config to function

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -26,6 +26,108 @@ local lsp_symbols = {
     Variable = "[] Variable",
 }
 
+local config = function()
+    local cmp = require "cmp"
+    cmp.setup {
+        view = {
+            entries = {
+                follow_cursor = true,
+            },
+        },
+        snippet = {
+            expand = function(args) require("luasnip").lsp_expand(args.body) end,
+        },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
+        mapping = {
+            ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+            ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+            ["<Down>"] = cmp.mapping.select_next_item {
+                behavior = cmp.SelectBehavior.Insert,
+            },
+            ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<CR>"] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Insert,
+                select = true,
+            },
+        },
+        formatting = {
+            format = function(entry, item)
+                item.kind = lsp_symbols[item.kind]
+                item.menu = ({
+                    nvim_lsp = "[LSP]",
+                    path = "[F]",
+                    luasnip = "[S]",
+                    buffer = "[B]",
+                    nvim_lua = "[Lua]",
+                    git = "[Git]",
+                    rg = "[RG]",
+                })[entry.source.name]
+                return item
+            end,
+        },
+        sources = cmp.config.sources {
+            { name = "nvim_lsp_signature_help" },
+            { name = "nvim_lsp", priority = 20, max_item_count = 15 },
+            { name = "luasnip", priority = 15 },
+            {
+                name = "rg",
+                option = {
+                    additional_arguments = "--hidden --smart-case --glob '!.git/'",
+                    context_before = 5,
+                    context_after = 5,
+                },
+                keyword_length = 3,
+                max_item_count = 5,
+                priority = 2,
+            },
+            {
+                name = "buffer",
+                keyword_length = 3,
+                max_item_count = 5,
+                priority = -2,
+                option = {
+                    get_bufnrs = function() return vim.api.nvim_list_bufs() end,
+                    keyword_pattern = [[\k\+]],
+                },
+            },
+            { name = "path" },
+            { name = "nvim_lua" },
+        },
+        experimental = {
+            ghost_text = true,
+        },
+    }
+    cmp.setup.filetype("gitcommit", {
+        sources = cmp.config.sources({
+            { name = "git" },
+        }, {
+            { name = "buffer" },
+        }),
+    })
+    cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+            { name = "path" },
+        }, {
+            { name = "cmdline" },
+        }),
+    })
+
+    cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+            { name = "buffer" },
+        },
+    })
+end
+
 return {
     {
         "hrsh7th/nvim-cmp",
@@ -42,101 +144,6 @@ return {
             { "hrsh7th/cmp-cmdline" },
             { "hrsh7th/cmp-nvim-lsp-signature-help" },
         },
-        config = function()
-            local cmp = require "cmp"
-            cmp.setup {
-                snippet = {
-                    expand = function(args) require("luasnip").lsp_expand(args.body) end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = {
-                    ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
-                    ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
-                    ["<Down>"] = cmp.mapping.select_next_item {
-                        behavior = cmp.SelectBehavior.Insert,
-                    },
-                    ["<Up>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm {
-                        behavior = cmp.ConfirmBehavior.Insert,
-                        select = true,
-                    },
-                },
-                formatting = {
-                    format = function(entry, item)
-                        item.kind = lsp_symbols[item.kind]
-                        item.menu = ({
-                            nvim_lsp = "[LSP]",
-                            path = "[F]",
-                            luasnip = "[S]",
-                            buffer = "[B]",
-                            nvim_lua = "[Lua]",
-                            git = "[Git]",
-                            rg = "[RG]",
-                        })[entry.source.name]
-                        return item
-                    end,
-                },
-                sources = cmp.config.sources {
-                    { name = "nvim_lsp_signature_help" },
-                    { name = "nvim_lsp", priority = 20, max_item_count = 15 },
-                    { name = "luasnip", priority = 15 },
-                    {
-                        name = "rg",
-                        option = {
-                            additional_arguments = "--hidden --smart-case --glob '!.git/'",
-                            context_before = 5,
-                            context_after = 5,
-                        },
-                        keyword_length = 3,
-                        max_item_count = 5,
-                        priority = 2,
-                    },
-                    {
-                        name = "buffer",
-                        keyword_length = 3,
-                        max_item_count = 5,
-                        priority = -2,
-                        option = {
-                            get_bufnrs = function() return vim.api.nvim_list_bufs() end,
-                            keyword_pattern = [[\k\+]],
-                        },
-                    },
-                    { name = "path" },
-                    { name = "nvim_lua" },
-                },
-                experimental = {
-                    ghost_text = true,
-                },
-            }
-            cmp.setup.filetype("gitcommit", {
-                sources = cmp.config.sources({
-                    { name = "git" },
-                }, {
-                    { name = "buffer" },
-                }),
-            })
-            cmp.setup.cmdline(":", {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = "path" },
-                }, {
-                    { name = "cmdline" },
-                }),
-            })
-
-            cmp.setup.cmdline({ "/", "?" }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = {
-                    { name = "buffer" },
-                },
-            })
-        end,
+        config = config,
     },
 }
