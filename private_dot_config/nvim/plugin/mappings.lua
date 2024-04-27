734126034f7e8dde10c5d103e518940e53e2c751
Rafael Madriz <rafa@rafaelmadriz.com>
2024-04-27 19:22:47 -0300

    nvim.mappings: fix `:Q`

--- private_dot_config/nvim/plugin/mappings.lua
+++ private_dot_config/nvim/plugin/mappings.lua
@@ -6,6 +6,8 @@ map({ "n", "x" }, "k", [[v:count == 0 ? 'gk' : 'k']], { expr = true })
 
 -- Disable repeat last recorded character
 map("n", "Q", "<Nop>")
+-- I always type :Q by mistake
+vim.api.nvim_create_user_command("Q", "quit", {})
 
 -- Center buffer when searching
 --map("n", "n", "nzzzv")
