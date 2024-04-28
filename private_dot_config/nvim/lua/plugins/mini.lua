984ecf3233d22d6b33f09e4b25819129aad5e672
Rafael Madriz <rafa@rafaelmadriz.com>
2024-04-28 17:12:32 -0300

    nvim.mini: install as library instead of standalone plugin

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -1,24 +1,27 @@
+local M = {}
+
+M.ai = function()
+    local ai = require "mini.ai"
+    require("mini.ai").setup {
+        n_lines = 500,
+        mappings = { around_last = "", inside_last = "" },
+        custom_textobjects = {
+            i = ai.gen_spec.treesitter({ a = "@conditional.outer", i = "@conditional.inner" }, {}),
+            f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+            c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+        },
+    }
+end
+
 return {
     {
-        "echasnovski/mini.ai",
-        event = "VeryLazy",
+        "echasnovski/mini.nvim",
+        lazy = false,
+        version = false,
         dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
         config = function()
-            local ai = require "mini.ai"
-            require("mini.ai").setup {
-                n_lines = 500,
-                mappings = { around_last = "", inside_last = "" },
-                custom_textobjects = {
-                    i = ai.gen_spec.treesitter({ a = "@conditional.outer", i = "@conditional.inner" }, {}),
-                    f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-                    c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
-                },
-            }
+            M.ai()
+            require("mini.align").setup {}
         end,
-    },
-    {
-        "echasnovski/mini.align",
-        keys = { { "ga", mode = { "n", "v" } }, { "gA", mode = { "n", "v" } } },
-        config = function() require("mini.align").setup {} end,
     },
 }
