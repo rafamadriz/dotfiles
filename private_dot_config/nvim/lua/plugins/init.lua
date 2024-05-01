1b7e9ff5833a99a2eaab5cc781fabe456d1b98ab
Rafael Madriz <rafa@rafaelmadriz.com>
2024-04-30 21:51:18 -0300

    nvim.undotree: change layout to open from right

--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -188,7 +188,7 @@ return {
         cmd = "UndotreeToggle",
         keys = { { "<leader>u", "<cmd>UndotreeToggle<CR>", desc = "Undotree" } },
         config = function()
-            vim.g.undotree_WindowLayout = 2
+            vim.g.undotree_WindowLayout = 4
             vim.g.undotree_SplitWidth = 45
             vim.g.undotree_SetFocusWhenToggle = 1
         end,
