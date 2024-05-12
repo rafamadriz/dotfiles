b55773a81969153623a17554fc4309c0fc484f02
Rafael Madriz <rafa@rafaelmadriz.com>
2024-05-12 16:08:38 -0300

    fix(nvim.cmp): use <C-y> to confirm
    TLDR: don't use the same keymap for two different things in the same mode.
    
    Using `<CR>` just causes inconsistent behaviour, since it's already used in
    insert mode to insert new line, if the cmp menu is shown then in inserts
    the first completion candidate, but sometimes you actually want to insert a
    new line instead of confirm, this happens a lot when writing text and not code:
    
    - you write `word`
    - cmp suggests `words`
    - hit enter for new line
    - cmp confirms `words`...

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -52,7 +52,7 @@ local config = function()
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.abort(),
-            ["<CR>"] = cmp.mapping.confirm {
+            ["<C-y>"] = cmp.mapping.confirm {
                 behavior = cmp.ConfirmBehavior.Insert,
                 select = true,
             },
