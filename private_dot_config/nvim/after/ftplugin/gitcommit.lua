51ce043cc8a7e172d11bc282bd4531defd60bb7d
Rafael Madriz <rafa@rafaelmadriz.com>
2024-05-12 00:56:32 -0300

    feat(git): add folds to comments and misc section
    * add foldmarkers to each section and start with all folds closed
      in neovim options
    
    * add misc section for links to conventional commits examples and
      specification

--- private_dot_config/nvim/after/ftplugin/gitcommit.lua
+++ private_dot_config/nvim/after/ftplugin/gitcommit.lua
@@ -4,3 +4,6 @@ vim.opt_local.relativenumber = false
 vim.opt_local.spell = true
 vim.opt_local.spelllang = "en_gb"
 vim.opt_local.textwidth = 72
+vim.opt.foldmethod = "marker"
+vim.opt.foldmarker = "{{{,}}}"
+vim.opt.foldlevel = 0
