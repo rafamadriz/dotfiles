7fadefe1cbe756f62d4e4e9d01e85c90ef1a4a7a
Rafael Madriz <rafa@rafaelmadriz.com>
2024-05-16 19:16:21 -0300

    fix(nvim.bigfile): return 0 instead of nil
    Returning `get_buf_size()` with nil will result in an error when
    comparing with integer, default to 0.
    
    Error would happen when opening quickfix buffers.

--- private_dot_config/nvim/plugin/bigfile.lua
+++ private_dot_config/nvim/plugin/bigfile.lua
@@ -3,7 +3,7 @@
 local function get_buf_size(bufnr)
     bufnr = bufnr or vim.api.nvim_get_current_buf()
     local ok, stats = pcall(function() return vim.loop.fs_stat(vim.api.nvim_buf_get_name(bufnr)) end)
-    if not (ok and stats) then return end
+    if not (ok and stats) then return 0 end
     return math.floor(0.5 + (stats.size / (1024 * 1024)))
 end
 
