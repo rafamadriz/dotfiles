d5c457ba52ac22f89cf49310672599a47579cf88
Rafael Madriz <rafa@rafaelmadriz.com>
2024-05-28 07:09:57 -0300

    fix(nvim): Unhandled method textDocument/diagnostic
    I hope this is a temporal fix for https://github.com/rafamadriz/dotfiles/issues/2
    
    * remove redundant `cmp.config.sources` in cmp sources configuration

--- private_dot_config/nvim/lua/plugins/lspconfig/init.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig/init.lua
@@ -40,9 +40,59 @@ return {
                         }
                     end
 
+                    -- Fix for https://github.com/rafamadriz/dotfiles/issues/2
+                    local capabilities = vim.lsp.protocol.make_client_capabilities()
+                    capabilities.textDocument.completion = {
+                        dynamicRegistration = false,
+                        completionItem = {
+                            snippetSupport = true,
+                            commitCharactersSupport = true,
+                            deprecatedSupport = true,
+                            preselectSupport = true,
+                            tagSupport = {
+                                valueSet = {
+                                    1,
+                                },
+                            },
+                            insertReplaceSupport = true,
+                            resolveSupport = {
+                                properties = {
+                                    "documentation",
+                                    "detail",
+                                    "additionalTextEdits",
+                                    "sortText",
+                                    "filterText",
+                                    "insertText",
+                                    "textEdit",
+                                    "insertTextFormat",
+                                    "insertTextMode",
+                                },
+                            },
+                            insertTextModeSupport = {
+                                valueSet = {
+                                    1,
+                                    2,
+                                },
+                            },
+                            labelDetailsSupport = true,
+                        },
+                        contextSupport = true,
+                        insertTextMode = 1,
+                        completionList = {
+                            itemDefaults = {
+                                "commitCharacters",
+                                "editRange",
+                                "insertTextFormat",
+                                "insertTextMode",
+                                "data",
+                            },
+                        },
+                    }
+
                     require("lspconfig.ui.windows").default_options.border = "rounded"
                     lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
-                        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                        -- capabilities = require("cmp_nvim_lsp").default_capabilities(),
+                        capabilities = capabilities,
                     })
                 end,
             },
