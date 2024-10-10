b9237627c9f794b0490722805f1c1356e62c7d9e
Rafael Madriz <rafa@rafaelmadriz.com>
2024-10-10 17:03:47 -0300

    refactor(nvim): some options

--- private_dot_config/nvim/lua/plugins/oil.lua
+++ private_dot_config/nvim/lua/plugins/oil.lua
@@ -7,6 +7,12 @@ return {
         oil.setup {
             default_file_explorer = true,
             delete_to_trash = true,
+            columns = {
+                -- "icon",
+                -- "permissions",
+                -- "size",
+                -- "mtime",
+            },
             view_options = {
                 show_hidden = true,
             },
