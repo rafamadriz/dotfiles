7b35a3d36a5951f35ba28afab8add9f9dfe99f8d
Rafael Madriz <rafa@rafaelmadriz.com>
2025-02-09 23:36:25 -0300

    feat(nvim): replace lualine with mini.statusline
    Reducing plugins with modules that are already in mini.nvim part 1

--- private_dot_config/nvim/lua/plugins/mini.lua
+++ private_dot_config/nvim/lua/plugins/mini.lua
@@ -51,6 +51,69 @@ M.git = function()
     })
 end
 
+M.statusline = function()
+    local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+    local git = MiniStatusline.section_git { trunc_width = 40 }
+    local diff = MiniStatusline.section_diff { trunc_width = 75 }
+
+    local create_hl = function(severity)
+        vim.api.nvim_set_hl(0, "MiniStatuslineDiagnostic" .. severity, {
+            bg = vim.api.nvim_get_hl(0, { name = "MiniStatuslineFilename" }).bg,
+            fg = vim.api.nvim_get_hl(0, { name = "Diagnostic" .. severity }).fg,
+            bold = true,
+        })
+    end
+
+    for _, severity in pairs { "Error", "Warn", "Info", "Hint" } do
+        create_hl(severity)
+    end
+
+    local diag_signs = {
+        ERROR = "%#MiniStatuslineDiagnosticError#E:",
+        WARN = "%#MiniStatuslineDiagnosticWarn#W:",
+        INFO = "%#MiniStatuslineDiagnosticInfo#I:",
+        HINT = "%#MiniStatuslineDiagnosticHint#H:",
+    }
+    -- Compute colored diagnostics and reset color for later LSP section
+    local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75, signs = diag_signs }
+    diagnostics = diagnostics .. "%#MiniStatuslineFilename#"
+
+    local filename = MiniStatusline.section_filename { trunc_width = 140 }
+    local fileinfo = string.format("%s | %s", vim.opt.fileencoding:get(), vim.bo.fileformat)
+
+    local location = function()
+        local pos = vim.fn.getcurpos()
+        local line, column = pos[2], pos[3]
+        local height = vim.api.nvim_buf_line_count(0)
+
+        local str = " "
+        local padding = #tostring(height) - #tostring(line)
+        if padding > 0 then str = str .. (" "):rep(padding) end
+
+        str = str .. "ℓ "
+        str = str .. line
+        str = str .. " c "
+        str = str .. column
+        str = str .. " "
+
+        if #tostring(column) < 2 then str = str .. " " end
+        return str
+    end
+
+    local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
+    return MiniStatusline.combine_groups {
+        { hl = mode_hl, strings = { mode } },
+        { hl = "MiniStatuslineDevinfo", strings = { git, diff } },
+        "%<", -- Mark general truncate point
+        { hl = "MiniStatuslineFilename", strings = { filename } },
+        "%=", -- End left alignment
+        { hl = "MiniStatuslineFilename", strings = { diagnostics } },
+        { hl = "MiniStatuslineFileInfo", strings = { fileinfo } },
+        { hl = mode_hl, strings = { search, location() } },
+    }
+end
+
 return {
     {
         "echasnovski/mini.nvim",
@@ -63,6 +126,9 @@ return {
             M.notify()
             M.git()
             require("mini.align").setup {}
+            require("mini.statusline").setup {
+                content = { active = M.statusline },
+            }
         end,
     },
 }
