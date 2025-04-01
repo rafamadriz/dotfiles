8febeb6c177fad436abed2460c7166a44381c434
Rafael Madriz <rafa@rafaelmadriz.com>
2025-03-31 21:42:29 -0300

    refactor(nvim.autocommands): remove trailing space cleanup
    Remove autocommand that automatically deleted trailing whitespace and
    empty lines at end of file on buffer write.

--- private_dot_config/nvim/plugin/autocommands.lua
+++ private_dot_config/nvim/plugin/autocommands.lua
@@ -99,39 +99,6 @@ aucmd({ "Filetype" }, {
     end,
 })
 
--- Source: https://github.com/mcauley-penney/tidy.nvim
-aucmd("BufWritePre", {
-    pattern = "*",
-    desc = "Delete trailing white space",
-    group = augroup("TrimTrailingSpace", { clear = true }),
-    callback = function()
-        local pos = vim.api.nvim_win_get_cursor(0)
-
-        -- delete trailing whitespace
-        vim.cmd [[:keepjumps keeppatterns %s/\s\+$//e]]
-
-        -- delete lines @ eof
-        vim.cmd [[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]]
-
-        local num_rows = vim.api.nvim_buf_line_count(0)
-
-        --[[
-            if the row value in the original cursor
-            position tuple is greater than the
-            line count after empty line deletion
-            (meaning that the cursor was inside of
-            the group of empty lines at the end of
-            the file when they were deleted), set
-            the cursor row to the last line.
-        ]]
-        if pos[1] > num_rows then
-            pos[1] = num_rows
-        end
-
-        vim.api.nvim_win_set_cursor(0, pos)
-    end,
-})
-
 -- HLSEARCH
 --source: https://github.com/akinsho/dotfiles/blob/main/.config/nvim/plugin/autocommands.lua
 --credits: @akinsho
