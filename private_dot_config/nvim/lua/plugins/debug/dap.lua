a869e128ae8ede74e4e946cc727801488cfce80e
Rafael Madriz <rafa@rafaelmadriz.com>
2025-09-28 22:19:00 -0300

    feat(nvim.debug): add C config for debugging

--- private_dot_config/nvim/lua/plugins/debug/dap.lua
+++ private_dot_config/nvim/lua/plugins/debug/dap.lua
@@ -30,6 +30,37 @@ return {
     "mfussenegger/nvim-dap",
     dependencies = { M.dapui },
     lazy = false,
+    config = function()
+        local dap = require "dap"
+
+        dap.adapters.cppdbg = {
+            id = "cppdbg",
+            type = "executable",
+            command = "OpenDebugAD7",
+        }
+
+        dap.configurations.c = {
+            {
+                name = "Launch file",
+                type = "cppdbg",
+                request = "launch",
+                program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+                cwd = "${workspaceFolder}",
+                stopAtEntry = true,
+            },
+            {
+                name = "Attach to gdbserver :1234",
+                type = "cppdbg",
+                request = "launch",
+                MIMode = "gdb",
+                miDebuggerServerAddress = "localhost:1234",
+                miDebuggerPath = "/usr/bin/gdb",
+                cwd = "${workspaceFolder}",
+                program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+            },
+        }
+    end,
+
     keys = {
         {
             "<leader>dB",
