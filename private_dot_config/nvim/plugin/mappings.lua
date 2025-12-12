db8c2145ded24fa965e45704d444addc6aa3a988
Rafael Madriz <rafa@rafaelmadriz.com>
2025-12-12 18:18:25 -0300

    feat(nvim.mappings): add plugin keymaps for fzf
    * Reorganize keympas
    * delete `CopyCodeBlock` command
    * add <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
    * add `RemoveTrailing` command

--- private_dot_config/nvim/plugin/mappings.lua
+++ private_dot_config/nvim/plugin/mappings.lua
@@ -4,49 +4,76 @@ local map = vim.keymap.set
 map({ "n", "x" }, "j", [[v:count == 0 ? 'gj' : 'j']], { expr = true })
 map({ "n", "x" }, "k", [[v:count == 0 ? 'gk' : 'k']], { expr = true })
 
--- Kinda of smooth scrolling, help scroll-smooth
+map("n", "<BS>", "<C-^>", { desc = "Jump between last two buffers" })
+
+map({ "n", "v" }, "H", "^", { desc = "Go to first character of line" })
+map({ "n", "v" }, "L", "g_", { desc = "Go to last character of line" })
+
+map("n", "<leader>u", ":Undotree<CR>", { desc = "undotree" })
+
+-- terminal
+map("t", [[<C-\>]], [[<C-\><C-n>]], { desc = "Enter normal mode in terminal" })
+
+-- Disable repeat last recorded character
+map("n", "Q", "<Nop>")
+
+-- visual
+map(
+    "n",
+    "gV",
+    [["`[" . strpart(getregtype(), 0, 1) . "`]"]],
+    { expr = true, desc = "Reselect latest changed, put or yanked text" }
+)
+map("x", "/", "<esc>/\\%V", { silent = false, desc = "Search inside visual selection" })
+
+-- Fix annoying behavior where for example: If in blockwise visual mode
+-- and add some text through multiple lines, I would end with <C-c>
+-- (out of muscle memory), but this cancels whatever I did and have to do it again
+-- source: https://github.com/neovim/neovim/issues/16416
+map("i", "<C-c>", "<C-c>")
+
+-- Kinda of smooth scrolling. See :help scroll-smooth
 map("n", "<C-U>", "<C-Y><C-Y><C-Y><C-Y><C-Y><C-Y><C-Y><C-Y><C-Y><C-Y><C-Y>")
 map("n", "<C-D>", "<C-E><C-E><C-E><C-E><C-E><C-E><C-E><C-E><C-E><C-E><C-E>")
+
+-- Copy/paste with system clipboard
+map({ "n", "x", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+map("n", "<leader>Y", [["+y$]], { desc = "Yank to system clipboard (end of line)" })
+map("n", "<leader>p", [["+p`[v`]=]], { desc = "Paste from system clipboard" })
+map("n", "<leader>P", [["+P]], { desc = "Paste from system clipboard" })
+-- Paste in Visual with `P` to not copy selected text (`:h v_P`)
+map("x", "<leader>p", [["+P]], { desc = "Paste from system clipboard" })
+
+-- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
+map({ "t", "i" }, "<A-h>", "<C-\\><C-n><C-w>h")
+map({ "t", "i" }, "<A-j>", "<C-\\><C-n><C-w>j")
+map({ "t", "i" }, "<A-k>", "<C-\\><C-n><C-w>k")
+map({ "t", "i" }, "<A-l>", "<C-\\><C-n><C-w>l")
+map({ "n" }, "<A-h>", "<C-w>h")
+map({ "n" }, "<A-j>", "<C-w>j")
+map({ "n" }, "<A-k>", "<C-w>k")
+map({ "n" }, "<A-l>", "<C-w>l")
+
+-- Save and delete buffers
+map("n", "<leader>w", ":update<CR>", { desc = "Write buffer" })
+map("v", "<leader>w", "<ESC>:update<CR>gv", { desc = "Write buffer" })
+map("n", "<leader>q", ":bdelete<CR>", { desc = "Quit buffer" })
+map("n", "<leader>Q", ":write | silent %bd | silent e# | bd#<CR>", { desc = "Quit all buffers but current" })
+map("n", "<leader>!", ":noautocmd write<CR>", { desc = "Save noautocmd" })
 
 -- Easier builtin completion
 map("i", "<c-space>", "<C-X><C-O>")
 map("i", "<C-F>", "<C-X><C-F>")
 map("i", "<C-L>", "<C-X><C-L>")
 
--- Terminal
-map("t", [[<C-\>]], [[<C-\><C-n>]])
+-- Use `<C-g>u` in Insert mode to mark this as separate undoable action.
+-- Source: https://stackoverflow.com/a/16481737
+-- NOTE: this remaps `<C-z>` in Normal mode (completely stops Neovim)
+map("n", "<C-Z>", "[s1z=", { desc = "Correct latest misspelled word" })
+map("i", "<C-Z>", "<C-g>u<Esc>[s1z=`]a<C-g>u", { desc = "Correct latest misspelled word" })
 
--- Disable repeat last recorded character
-map("n", "Q", "<Nop>")
--- I always type :Q by mistake
-vim.api.nvim_create_user_command("Q", function(arg)
-    if arg.bang then
-        vim.cmd.quit { bang = true }
-    else
-        vim.cmd.quit()
-    end
-end, { bang = true })
-
--- I always type :W
-vim.api.nvim_create_user_command("W", function(arg)
-    if arg.bang then
-        vim.cmd.write { bang = true }
-    else
-        vim.cmd.update()
-    end
-end, { bang = true })
-
--- Center buffer when searching
---map("n", "n", "nzzzv")
---map("n", "N", "Nzzzv")
-
--- `^` to go to beginning of line with a character is not convenient, never use `H` in normal mode,
--- so why not use it for that? Might as well add `L` for end of line to keep it consistent
-map({ "n", "v" }, "H", "^")
-map({ "n", "v" }, "L", "g_")
-
--- Keep cursor position when joining lines
-map("n", "J", "mzJ`z", { desc = "Join lines" })
+map("v", "<", "<gv", { desc = "Keep visual selection when indenting to left" })
+map("v", ">", ">gv", { desc = "Keep visual selection when indenting to right" })
 
 -- Make sure to go to proper indentation level when pressing i
 -- source: https://www.reddit.com/r/neovim/comments/12rqyl8/5_smart_minisnippets_for_making_text_editing_more/
@@ -58,69 +85,50 @@ map("n", "i", function()
     end
 end, { expr = true })
 
--- Jump between last two buffers
-map("n", "<BS>", "<C-^>", { desc = "Jump between last two buffers" })
+-- I always type :Q by mistake
+vim.api.nvim_create_user_command("Q", function(arg)
+    if arg.bang then
+        vim.cmd.quit { bang = true }
+    else
+        vim.cmd.quit()
+    end
+end, { bang = true })
 
--- Fix annoying behavior where for example: If in blockwise visual mode
--- and add some text through multiple lines, I would end with <C-c>
--- (out of muscle memory), but this cancels whatever I did and have to do it again
--- source: https://github.com/neovim/neovim/issues/16416
-map("i", "<C-c>", "<C-c>")
+-- I always type :W by mistake
+vim.api.nvim_create_user_command("W", function(arg)
+    if arg.bang then
+        vim.cmd.write { bang = true }
+    else
+        vim.cmd.update()
+    end
+end, { bang = true })
 
--- Move selected block of text up/down
-map("v", "K", ":move '<-2<CR>gv=gv", { desc = "Move selected block of text up" })
-map("v", "J", ":move '>+1<CR>gv=gv", { desc = "Move selected block of text down" })
+vim.api.nvim_create_user_command("RemoveTrailing", function()
+    local pos = vim.api.nvim_win_get_cursor(0)
 
--- Copy/paste with system clipboard
-map({ "n", "x", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
-map("n", "<leader>Y", [["+y$]], { desc = "Yank to system clipboard (end of line)" })
-map("n", "<leader>p", [["+p`[v`]=]], { desc = "Paste from system clipboard" })
-map("n", "<leader>P", [["+P]], { desc = "Paste from system clipboard" })
--- Paste in Visual with `P` to not copy selected text (`:h v_P`)
-map("x", "<leader>p", [["+P]], { desc = "Paste from system clipboard" })
+    -- delete trailing whitespace
+    vim.cmd [[:keepjumps keeppatterns %s/\s\+$//e]]
 
--- Reselect latest changed, put, or yanked text
-map("n", "gV", [["`[" . strpart(getregtype(), 0, 1) . "`]"]], { expr = true, desc = "Visually select changed text" })
+    -- delete lines at eof
+    vim.cmd [[:keepjumps keeppatterns silent! 0;/^\%(\n*.\)\@!/,$d_]]
 
--- Search inside visually highlighted text. Use `silent = false` for it to
--- make effect immediately.
-map("x", "/", "<esc>/\\%V", { silent = false, desc = "Search inside visual selection" })
+    local num_rows = vim.api.nvim_buf_line_count(0)
 
--- Search visually selected text (slightly better than builtins in Neovim>=0.8)
-map("x", "*", [[y/\V<C-R>=escape(@", '/\')<CR><CR>]])
-map("x", "#", [[y?\V<C-R>=escape(@", '?\')<CR><CR>]])
+    --[[
+            if the row value in the original cursor
+            position tuple is greater than the
+            line count after empty line deletion
+            (meaning that the cursor was inside of
+            the group of empty lines at the end of
+            the file when they were deleted), set
+            the cursor row to the last line.
+        ]]
+    if pos[1] > num_rows then
+        pos[1] = num_rows
+    end
 
--- Correct latest misspelled word by taking first suggestion.
--- Use `<C-g>u` in Insert mode to mark this as separate undoable action.
--- Source: https://stackoverflow.com/a/16481737
--- NOTE: this remaps `<C-z>` in Normal mode (completely stops Neovim), but
--- it seems to be too harmful anyway.
-map("n", "<C-Z>", "[s1z=", { desc = "Correct latest misspelled word" })
-map("i", "<C-Z>", "<C-g>u<Esc>[s1z=`]a<C-g>u", { desc = "Correct latest misspelled word" })
-
--- Some useful mappings when doing stuff in visual mode
-map("v", "y", "y`]", { desc = "Keep cursor position when yanking text" })
-map("v", "<", "<gv", { desc = "Keep visual selection when indenting to left" })
-map("v", ">", ">gv", { desc = "Keep visual selection when indenting to right" })
-
--- Move sideways in command mode. Using `silent = false` makes movements
--- to be immediately shown.
-map("c", "<C-h>", "<Left>", { silent = false, desc = "Left" })
-map("c", "<C-l>", "<Right>", { silent = false, desc = "Right" })
-
--- Move to start/end on line in command mode
-map("c", "<C-a>", "<home>", { desc = "Move to end of line" })
-map("c", "<C-e>", "<end>", { desc = "Move to begining of line" })
-
-map("n", "<leader>;", "mmA;<ESC>`m", { desc = "Apend semicolon" })
-map("n", "<leader>,", "mmA,<ESC>`m", { desc = "Apend comma" })
-
--- Save and delete buffers
-map("n", "<leader>w", ":update<CR>", { desc = "Write buffer" })
-map("v", "<leader>w", "<ESC>:update<CR>gv", { desc = "Write buffer" })
-map("n", "<leader>q", ":bdelete<CR>", { desc = "Quit buffer" })
-map("n", "<leader>Q", ":write | silent %bd | silent e# | bd#<CR>", { desc = "Quit all buffers but current" })
-map("n", "<leader>!", ":noautocmd write<CR>", { desc = "Save noautocmd" })
+    vim.api.nvim_win_set_cursor(0, pos)
+end, {})
 
 -- Resize windows
 -- expand or minimize current buffer in "actual" direction
@@ -154,63 +162,47 @@ map({ "n", "t" }, "<C-Down>", function() resize(false, 2) end, { desc = "Resize 
 map({ "n", "t" }, "<C-Left>", function() resize(true, -2) end, { desc = "Resize left window vertically" })
 map({ "n", "t" }, "<C-Right>", function() resize(true, 2) end, { desc = "Resize right window vertically" })
 
--- Copy text to clipboard using codeblock format ```{ft}{content}```
--- Source: https://www.reddit.com/r/neovim/comments/1dfvluw/share_your_favorite_settingsfeaturesexcerpts_from/
-vim.api.nvim_create_user_command("CopyCodeBlock", function(opts)
-    local lines = vim.api.nvim_buf_get_lines(0, opts.line1 - 1, opts.line2, true)
-    local content = table.concat(lines, "\n")
-    local result = string.format("```%s\n%s\n```", vim.bo.filetype, content)
-    vim.fn.setreg("+", result)
-    -- vim.notify 'Text copied to clipboard'
-end, { range = true })
-
--- jump to next/prev file
-local navigate_files = function(direction)
-    local current_file = vim.fn.expand "%:p"
-    local current_dir = vim.fn.expand "%:p:h"
-
-    -- get all files in the directory, sorted
-    local files = vim.fn.glob(current_dir .. "/*", false, true)
-    files = vim.tbl_filter(function(file) return vim.fn.isdirectory(file) == 0 end, files)
-    table.sort(files)
-
-    if #files == 0 then
-        -- no files in directory
+------------------------------
+------- Plugins Keymaps ------
+------------------------------
+-- FZF
+map("n", "<leader><leader>", function()
+    local current_buffer = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+    local is_oil_buffer = vim.startswith(current_buffer, "oil://")
+    if  is_oil_buffer then
+        vim.cmd(string.format("FzfLua files cwd=%s", require("oil").get_current_dir()))
         return
     end
-
-    local target_index
-
-    if direction == "first" then
-        target_index = 1
-    elseif direction == "last" then
-        target_index = #files
-    else
-        local index = vim.fn.index(files, current_file) + 1
-        if index <= 0 then
-            return
-        end
-
-        if direction == "next" then
-            target_index = index + 1
-            if target_index > #files then
+    vim.cmd "FzfLua global"
+end, { desc = "find global" })
+map("n", "<leader>`", ":FzfLua resume<CR>", { desc = "picker resume" })
+map("n", "<leader>fa", ":FzfLua builtin<CR>", { desc = "all pickers" })
+map("n", "<leader>?", ":FzfLua helptags<CR>", { desc = "help tags" })
+map("n", "<leader>fq", ":FzfLua quickfix<CR>", { desc = "quickfix" })
+map("n", "<leader>fg", ":FzfLua live_grep<CR>", { desc = "grep live" })
+map("n", "<leader>fw", ":FzfLua grep_cword<CR>", { desc = "grep cword" })
+map("v", "<leader>fv", ":FzfLua grep_visual<CR>", { desc = "grep visual" })
+map("n", "<leader>fp", ":FzfLua grep<CR>", { desc = "grep pattern" })
+map("n", "<leader>fd", function()
+    vim.ui.input(
+        { prompt = "path to directory (leave empty for directory of current file): ", completion = "dir" },
+        function(path)
+            if not path then
                 return
             end
-        elseif direction == "prev" then
-            target_index = index - 1
-            if target_index < 1 then
-                return
+            if path == "" then
+                local filename = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+                path = vim.fn.fnamemodify(filename, ":p:h")
             end
-        else
-            -- Invalid direction
-            return
+            vim.cmd(string.format("FzfLua files cwd=%s", path))
         end
-    end
-
-    vim.cmd("e " .. files[target_index])
-end
-
-map("n", "[f", function() navigate_files "prev" end, { desc = "Previous file in directory" })
-map("n", "]f", function() navigate_files "next" end, { desc = "Next file in directory" })
-map("n", "[F", function() navigate_files "first" end, { desc = "First file in directory" })
-map("n", "]F", function() navigate_files "last" end, { desc = "Last file in directory" })
+    )
+end, { desc = "find files in directory" })
+-- git
+map("n", "<leader>gC", ":FzfLua git_commits<CR>", { desc = "commits" })
+map("n", "<leader>gc", ":FzfLua git_bcommits<CR>", { desc = "buffer commits" })
+-- lsp
+map("n", "<leader>ld", ":FzfLua lsp_document_diagnostics<CR>", { desc = "document diagnostics" })
+map("n", "<leader>lD", ":FzfLua lsp_workspace_diagnostics<CR>", { desc = "workspace diagnostics" })
+map("n", "<leader>ls", ":FzfLua lsp_document_symbols<CR>", { desc = "document symbols" })
+map("n", "<leader>lS", ":FzfLua lsp_workspace_symbols<CR>", { desc = "workspace symbols" })
