212a8d5888bd97da77b32920ee599149a42c0c4c
Rafael Madriz <rafa@rafaelmadriz.com>
2026-06-19 15:55:51 -0300

    feat(nvim.mini.git): align blame output with current window state
    Taken from `:help MiniGit-examples "Tweaking command output" section.

--- private_dot_config/nvim/lua/configs/mini.lua
+++ private_dot_config/nvim/lua/configs/mini.lua
@@ -88,6 +88,22 @@ vim.keymap.set("n", "<leader>gt", require("mini.diff").toggle_overlay, { desc = 
 vim.keymap.set("n", "<leader>gs", ":Git add -- %<CR>",                 { desc = "stage buffer" })
 vim.keymap.set("n", "<leader>gu", ":Git restore --staged -- %<CR>",    { desc = "unstage buffer" })
 
+local align_blame = function(au_data)
+    if au_data.data.git_subcommand ~= 'blame' then return end
+
+    -- Align blame output with source
+    local win_src = au_data.data.win_source
+    vim.wo.wrap = false
+    vim.fn.winrestview({ topline = vim.fn.line('w0', win_src) })
+    vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+
+    -- Bind both windows so that they scroll together
+    vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+end
+
+local au_opts = { pattern = 'MiniGitCommandSplit', callback = align_blame }
+vim.api.nvim_create_autocmd('User', au_opts)
+
 -- Snippets
 local snippets = require "mini.snippets"
 local gen_loader = snippets.gen_loader
